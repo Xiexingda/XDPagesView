55eb48671048d7a0775070750291806d72669c39
xiexingda <779656694@qq.com>
2019-01-21 13:18:09 +0800

    优化

--- XDPagesView/XDTitleItem.h
+++ XDPagesView/XDTitleItem.h
@@ -16,20 +16,10 @@
  赋值item
 
  @param title 标题
- @param needIcon 是否首个item需要图标
- @param iconImage 首个item图标
- @param selectedIconImage 首个item选中时的图标
- @param needFollowLine 是否需要跟踪条
- @param followLineColor 跟宗条颜色
- @param followPercent 跟踪条所占百分比
- @param titleFont 标题大小
- @param textColor 标题颜色
- @param textSelectedColor 选中时的标题颜色
- @param barTag 标识，增加灵活性
  @param index 每个item对应的index
  @param selectedIndex 当前选中的index
+ @param titleBarLayout 布局
  */
-
 - (void)configItemByTitle:(NSString *)title
                     index:(NSIndexPath *)index
             selectedIndex:(NSInteger)selectedIndex
