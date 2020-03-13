1c20db0c41be2bffc0b16f08871fd435ea5cb3f3
谢兴达 <xiexingda@zempty.com>
2020-03-13 09:45:51 +0800

    XDPagesView2.0

--- Root/AppDelegate.m
+++ Root/AppDelegate.m
@@ -9,12 +9,7 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
 
-@interface AppDelegate ()
-
-@end
-
 @implementation AppDelegate
-
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc]initWithFrame:UIScreen.mainScreen.bounds];
